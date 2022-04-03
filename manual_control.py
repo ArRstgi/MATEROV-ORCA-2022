194746b6a08dfb74affe0b992fefaf4b436b1af1
Arush Rastogi <arushrastogi2@outlook.com>
2022-04-03 18:19:28 +0800

    first commit from vscode

---
+++ manual_control.py
@@ -0,0 +1,68 @@
+# Import mavutil
+import multiprocessing
+from pymavlink import mavutil
+from multiprocessing import Process
+import time
+# Create the connection
+master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
+# Wait a heartbeat before sending commands
+master.wait_heartbeat()
+
+# Send a positive x value, negative y, negative z,
+# positive rotation and no button.
+# https://mavlink.io/en/messages/common.html#MANUAL_CONTROL
+# Warning: Because of some legacy workaround, z will work between [0-1000]
+# where 0 is full reverse, 500 is no output and 1000 is full throttle.
+# x,y and r will be between [-1000 and 1000].
+
+def forward():
+    master.mav.manual_control_send(
+
+        # The values correspond to forward/backwards, roll of bot,
+        # up/down of a bot, right/left turning 
+        # and buttons used (usually 0) respectively
+
+        master.target_system,
+        500,
+        0,
+        0,
+        0,
+        0)
+
+def stop():
+    master.mav.manual_control_send(
+
+        # The values correspond to forward/backwards, roll of bot,
+        # up/down of a bot, right/left turning 
+        # and buttons used (usually 0) respectively
+        
+        master.target_system,
+        0,
+        0,
+        0,
+        0,
+        0)
+
+if __name__ == '__main__':
+
+    def task1start(): 
+    
+        p = multiprocessing.Process(target=forward)
+        p.start()
+        time.sleep(1)
+        p.terminate()
+    
+    def task1stop():
+
+        p = multiprocessing.Process(target=stop)
+        p.start()
+        time.sleep(1)
+        p.terminate()
+
+
+
+
+
+
+
+
