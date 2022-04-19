efb40b772b485dc277249afd902a92518d3541d4
Arush Rastogi <arushrastogi2@outlook.com>
2022-04-20 00:38:45 +0800

    changed file names, refined/tested

--- manual_control.py
+++ manual_control.py
@@ -91,7 +91,33 @@ def task1stop():
     time.sleep(1)
     p.terminate()
 
+# Create a function to send RC values
+# More information about Joystick channels
+# here: https://www.ardusub.com/operators-manual/rc-input-and-output.html#rc-inputs
+def set_rc_channel_pwm(channel_id, pwm=1500):
+    """ Set RC channel pwm value
+    Args:
+        channel_id (TYPE): Channel ID
+        pwm (int, optional): Channel pwm value 1100-1900
+    """
+    if channel_id < 1 or channel_id > 18:
+        print("Channel does not exist.")
+        return
 
+    # Mavlink 2 supports up to 18 channels:
+    # https://mavlink.io/en/messages/common.html#RC_CHANNELS_OVERRIDE
+    rc_channel_values = [65535 for _ in range(18)]
+    rc_channel_values[channel_id - 1] = pwm
+    master.mav.rc_channels_override_send(
+        master.target_system,                # target_system
+        master.target_component,             # target_component
+        *rc_channel_values)                  # RC channel list, in microseconds.
+
+def right():
+    set_rc_channel_pwm(6,1700)
+
+def left():
+    set_rc_channel_pwm(6,1300)
 
 
 
